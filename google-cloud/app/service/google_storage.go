5a56d4b7d87c4d15e955edded2d3130694d96d6e
haku10 <isptry5689@gmail.com>
2021-11-14 17:23:59 +0900

    クレデンシャルをOSに仕込んだ物を使用&ソースコードの反映をコンテナに反映するように変更

--- google-cloud/app/service/google_storage.go
+++ google-cloud/app/service/google_storage.go
@@ -2,29 +2,25 @@ package service
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"log"
 	"os"
 
 	"cloud.google.com/go/storage"
-	"google.golang.org/api/option"
 )
 
 // StorageOutput -> GCSへのファイル出力
 func StorageOutput() {
-	credentialFilePath := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
-	fmt.Print(credentialFilePath)
 
 	// クライアントを作成する
 	ctx := context.Background()
-	client, err := storage.NewClient(ctx, option.WithCredentialsFile(credentialFilePath))
+	client, err := storage.NewClient(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// GCSオブジェクトを書き込むファイルの作成
-	f, err := os.Create("testfiles/sample.txt")
+	file, err := os.Create("testfiles/sample.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,7 +30,7 @@ func StorageOutput() {
 	folderName := os.Getenv("GOOGLE_STORAGE_FOLDER")
 	objectPath := folderName + "/sample.txt"
 	writer := client.Bucket(bucketName).Object(objectPath).NewWriter(ctx)
-	_, err = io.Copy(writer, f)
+	_, err = io.Copy(writer, file)
 	if err != nil {
 		log.Fatal(err)
 	}
